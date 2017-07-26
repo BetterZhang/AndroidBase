0b8e339ab20d16b885683ccfdce224d22da9e7be
BetterZhang <betterzhang.dev@gmail.com>
2017-07-26 14:46:42 +0800

    add BaseActivity and BaseFragment

---
+++ common/src/main/java/com/betterzhang/common/ui/base/BaseActivity.java
@@ -0,0 +1,184 @@
+package com.betterzhang.common.ui.base;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+/**
+ * Created by Android Studio.
+ * Author : zhangzhongqiang
+ * Email  : zhangzhongqiang@jsdttec.com
+ * Time   : 2017/07/26 下午 2:04
+ * Desc   : Activity基类
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    protected Context mContext;
+    protected Toast mToast;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getContentViewId());
+        mContext = this;
+
+        initView();
+        loadData();
+        addListener();
+    }
+
+    /**
+     * 设置页面布局layout
+     * @return
+     */
+    protected abstract int getContentViewId();
+
+    /**
+     * 绑定页面UI
+     */
+    protected void initView() {
+
+    }
+
+    /**
+     * 加载数据
+     */
+    protected void loadData() {
+
+    }
+
+    /**
+     * 添加UI事件监听
+     */
+    protected void addListener() {
+
+    }
+
+    /**
+     * 短暂显示Toast提示(来自res)
+     * @param resId
+     */
+    protected void showShortToast(final int resId) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mToast == null) {
+                    mToast = Toast.makeText(mContext, getResources().getString(resId), Toast.LENGTH_SHORT);
+                } else {
+                    mToast.setText(resId);
+                }
+                mToast.show();
+            }
+        });
+    }
+
+    /**
+     * 短暂显示Toast提示(来自String)
+     * @param text
+     */
+    protected void showShortToast(final String text) {
+        if (!TextUtils.isEmpty(text)) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (mToast == null) {
+                        mToast = Toast.makeText(mContext, text, Toast.LENGTH_SHORT);
+                    } else {
+                        mToast.setText(text);
+                    }
+                    mToast.show();
+                }
+            });
+        }
+    }
+
+    /**
+     * 长时间显示Toast提示(来自res)
+     * @param resId
+     */
+    protected void showLongToast(final int resId) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mToast == null) {
+                    mToast = Toast.makeText(mContext, getResources().getString(resId), Toast.LENGTH_LONG);
+                } else {
+                    mToast.setText(resId);
+                }
+                mToast.show();
+            }
+        });
+    }
+
+    /**
+     * 长时间显示Toast提示(来自String)
+     * @param text
+     */
+    protected void showLongToast(final String text) {
+        if (!TextUtils.isEmpty(text)) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (mToast == null) {
+                        mToast = Toast.makeText(mContext, text, Toast.LENGTH_LONG);
+                    } else {
+                        mToast.setText(text);
+                    }
+                    mToast.show();
+                }
+            });
+        }
+    }
+
+    /**
+     * Activity跳转（无参）
+     * @param cls
+     */
+    protected void startAnimActivity(Class<?> cls) {
+        this.startAnimActivity(cls, null);
+    }
+
+    /**
+     * Activity跳转（有参）
+     * @param cls
+     * @param bundle
+     */
+    protected void startAnimActivity(Class<?> cls, Bundle bundle) {
+        Intent intent = new Intent();
+        intent.setClass(mContext, cls);
+        if (bundle != null) {
+            intent.putExtras(bundle);
+        }
+        startActivity(intent);
+    }
+
+    /**
+     * Activity跳转 ForResult（无参）
+     * @param cls
+     * @param requestCode
+     */
+    protected void startAnimActivityForResult(Class<?> cls, int requestCode) {
+        this.startAnimActivityForResult(cls, null, requestCode);
+    }
+
+    /**
+     * Activity跳转 ForResult（有参）
+     * @param cls
+     * @param bundle
+     * @param requestCode
+     */
+    protected void startAnimActivityForResult(Class<?> cls, Bundle bundle, int requestCode) {
+        Intent intent = new Intent();
+        intent.setClass(mContext, cls);
+        if (bundle != null) {
+            intent.putExtras(bundle);
+        }
+        startActivityForResult(intent, requestCode);
+    }
+
+}
---
+++ common/src/main/java/com/betterzhang/common/ui/base/BaseFragment.java
@@ -0,0 +1,213 @@
+package com.betterzhang.common.ui.base;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+/**
+ * Created by Android Studio.
+ * Author : zhangzhongqiang
+ * Email  : zhangzhongqiang@jsdttec.com
+ * Time   : 2017/07/26 下午 2:17
+ * Desc   : Fragment基类
+ */
+
+public abstract class BaseFragment extends Fragment {
+
+    protected Context mContext;
+    protected AppCompatActivity mActivity;
+    protected Toast mToast;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mActivity = (AppCompatActivity) context;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getContext();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        int id = getContentViewId();
+        View view = inflater.inflate(id, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initView();
+        loadData();
+        addListener();
+    }
+
+    /**
+     * 设置页面布局layout
+     * @return
+     */
+    protected abstract int getContentViewId();
+
+    /**
+     * 绑定页面UI
+     */
+    protected void initView() {
+
+    }
+
+    /**
+     * 加载数据
+     */
+    protected void loadData() {
+
+    }
+
+    /**
+     * 添加UI事件监听
+     */
+    protected void addListener() {
+
+    }
+
+    /**
+     * 短暂显示Toast提示(来自res)
+     * @param resId
+     */
+    protected void showShortToast(final int resId) {
+        mActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mToast == null) {
+                    mToast = Toast.makeText(mContext, getResources().getString(resId), Toast.LENGTH_SHORT);
+                } else {
+                    mToast.setText(resId);
+                }
+                mToast.show();
+            }
+        });
+    }
+
+    /**
+     * 短暂显示Toast提示(来自String)
+     * @param text
+     */
+    protected void showShortToast(final String text) {
+        if (!TextUtils.isEmpty(text)) {
+            mActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (mToast == null) {
+                        mToast = Toast.makeText(mContext, text, Toast.LENGTH_SHORT);
+                    } else {
+                        mToast.setText(text);
+                    }
+                    mToast.show();
+                }
+            });
+        }
+    }
+
+    /**
+     * 长时间显示Toast提示(来自res)
+     * @param resId
+     */
+    protected void showLongToast(final int resId) {
+        mActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mToast == null) {
+                    mToast = Toast.makeText(mContext, getResources().getString(resId), Toast.LENGTH_LONG);
+                } else {
+                    mToast.setText(resId);
+                }
+                mToast.show();
+            }
+        });
+    }
+
+    /**
+     * 长时间显示Toast提示(来自String)
+     * @param text
+     */
+    protected void showLongToast(final String text) {
+        if (!TextUtils.isEmpty(text)) {
+            mActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (mToast == null) {
+                        mToast = Toast.makeText(mContext, text, Toast.LENGTH_LONG);
+                    } else {
+                        mToast.setText(text);
+                    }
+                    mToast.show();
+                }
+            });
+        }
+    }
+
+    /**
+     * Activity跳转（无参）
+     * @param cls
+     */
+    protected void startAnimActivity(Class<?> cls) {
+        this.startAnimActivity(cls, null);
+    }
+
+    /**
+     * Activity跳转（有参）
+     * @param cls
+     * @param bundle
+     */
+    protected void startAnimActivity(Class<?> cls, Bundle bundle) {
+        Intent intent = new Intent();
+        intent.setClass(mContext, cls);
+        if (bundle != null) {
+            intent.putExtras(bundle);
+        }
+        startActivity(intent);
+    }
+
+    /**
+     * Activity跳转 ForResult（无参）
+     * @param cls
+     * @param requestCode
+     */
+    protected void startAnimActivityForResult(Class<?> cls, int requestCode) {
+        this.startAnimActivityForResult(cls, null, requestCode);
+    }
+
+    /**
+     * Activity跳转 ForResult（有参）
+     * @param cls
+     * @param bundle
+     * @param requestCode
+     */
+    protected void startAnimActivityForResult(Class<?> cls, Bundle bundle, int requestCode) {
+        Intent intent = new Intent();
+        intent.setClass(mContext, cls);
+        if (bundle != null) {
+            intent.putExtras(bundle);
+        }
+        startActivityForResult(intent, requestCode);
+    }
+
+}
