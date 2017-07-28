2c90baa2331c8e56f9cceecd682ab8db3d0f64ed
BetterZhang <betterzhang.dev@gmail.com>
2017-07-28 10:06:29 +0800

    add SplashActivity

--- app/src/main/java/com/betterzhang/androidbase/ui/main/MainActivity.java
+++ app/src/main/java/com/betterzhang/androidbase/ui/main/MainActivity.java
@@ -1,7 +1,16 @@
-package com.betterzhang.androidbase;
+package com.betterzhang.androidbase.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.betterzhang.androidbase.R;
+
+/**
+ * Created by Android Studio.
+ * Author : zhangzhongqiang
+ * Email  : zhangzhongqiang@jsdttec.com
+ * Time   : 2017/07/28 上午 9:52
+ * Desc   : MainActivity
+ */
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/betterzhang/androidbase/ui/splash/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.betterzhang.androidbase.ui.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+import com.betterzhang.androidbase.R;
+import com.betterzhang.androidbase.ui.main.MainActivity;
+import java.util.concurrent.TimeUnit;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * Created by Android Studio.
+ * Author : zhangzhongqiang
+ * Email  : zhangzhongqiang@jsdttec.com
+ * Time   : 2017/07/28 上午 9:52
+ * Desc   : App启动页面
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 设置无标题
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 设置全屏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+        Observable.timer(2, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(aLong -> {
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                });
+    }
+}
