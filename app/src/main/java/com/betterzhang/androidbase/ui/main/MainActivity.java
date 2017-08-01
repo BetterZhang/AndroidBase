31aee0764febcddc1dbdeb4e4b08df0181048600
BetterZhang <betterzhang.dev@gmail.com>
2017-08-01 13:57:18 +0800

    MainActivity add exit function

--- app/src/main/java/com/betterzhang/androidbase/ui/main/MainActivity.java
+++ app/src/main/java/com/betterzhang/androidbase/ui/main/MainActivity.java
@@ -1,5 +1,9 @@
 package com.betterzhang.androidbase.ui.main;
 
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.FrameLayout;
 import com.betterzhang.androidbase.R;
 import com.betterzhang.common.ui.base.BaseActivity;
@@ -20,6 +24,8 @@ public class MainActivity extends BaseActivity {
     BottomBar bottomBar;
     @BindView(R.id.content)
     FrameLayout content;
+
+    private long exitTime = 0;
 
     @Override
     protected int getLayoutResId() {
@@ -49,5 +55,40 @@ public class MainActivity extends BaseActivity {
             }
             showShortToast(msg);
         });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (KeyEvent.KEYCODE_BACK == keyCode) {
+            // 判断是否在两秒之内连续点击返回键，是则退出，否则不退出
+            if (System.currentTimeMillis() - exitTime > 2000) {
+                Snackbar.make(content, getString(R.string.text_exit_app), Snackbar.LENGTH_SHORT)
+                        .setAction(getString(R.string.text_cancel), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                exitTime = 0;
+                            }
+                        })
+                        .setActionTextColor(ContextCompat.getColor(this, R.color.common_tab_text_selected))
+                        .show();
+                // 将系统当前的时间赋值给exitTime
+                exitTime = System.currentTimeMillis();
+            } else {
+                finish();
+                new Thread() {
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.exit(0);
+                    }
+                }.start();
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
