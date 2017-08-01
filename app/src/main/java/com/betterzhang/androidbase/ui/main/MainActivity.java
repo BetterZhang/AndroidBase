ff7a4d722c82c20ad70bd923f784110e7db0c661
BetterZhang <betterzhang.dev@gmail.com>
2017-08-01 14:33:11 +0800

    update BottomBar to BottomNavigationBar

--- app/src/main/java/com/betterzhang/androidbase/ui/main/MainActivity.java
+++ app/src/main/java/com/betterzhang/androidbase/ui/main/MainActivity.java
@@ -5,9 +5,10 @@ import android.support.v4.content.ContextCompat;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.FrameLayout;
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 import com.betterzhang.androidbase.R;
 import com.betterzhang.common.ui.base.BaseActivity;
-import com.roughike.bottombar.BottomBar;
 import butterknife.BindView;
 
 /**
@@ -18,10 +19,10 @@ import butterknife.BindView;
  * Desc   : MainActivity
  */
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements BottomNavigationBar.OnTabSelectedListener {
 
     @BindView(R.id.bottomBar)
-    BottomBar bottomBar;
+    BottomNavigationBar bottomBar;
     @BindView(R.id.content)
     FrameLayout content;
 
@@ -33,28 +34,30 @@ public class MainActivity extends BaseActivity {
     }
 
     @Override
+    protected void initView() {
+        super.initView();
+        bottomBar.setMode(BottomNavigationBar.MODE_FIXED);
+        bottomBar.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC);
+        bottomBar.addItem(new BottomNavigationItem(R.drawable.selector_tab_homepage, "首页"))
+                 .setActiveColor(R.color.common_tab_text_selected)
+                 .setInActiveColor(R.color.common_tab_text_unselected)
+                 .addItem(new BottomNavigationItem(R.drawable.selector_tab_market, "行情"))
+                 .setActiveColor(R.color.common_tab_text_selected)
+                 .setInActiveColor(R.color.common_tab_text_unselected)
+                 .addItem(new BottomNavigationItem(R.drawable.selector_tab_trade, "交易"))
+                 .setActiveColor(R.color.common_tab_text_selected)
+                 .setInActiveColor(R.color.common_tab_text_unselected)
+                 .addItem(new BottomNavigationItem(R.drawable.selector_tab_personal, "我的"))
+                 .setActiveColor(R.color.common_tab_text_selected)
+                 .setInActiveColor(R.color.common_tab_text_unselected)
+                 .setFirstSelectedPosition(0)
+                 .initialise();
+    }
+
+    @Override
     protected void addListener() {
         super.addListener();
-        bottomBar.setOnTabSelectListener(tabId -> {
-            String msg = "";
-            switch (tabId) {
-                case R.id.tab_home_page:
-                    msg = "首页";
-                    break;
-                case R.id.tab_home_market:
-                    msg = "行情";
-                    break;
-                case R.id.tab_home_trade:
-                    msg = "交易";
-                    break;
-                case R.id.tab_home_personal:
-                    msg = "我的";
-                    break;
-                default:
-                    break;
-            }
-            showShortToast(msg);
-        });
+        bottomBar.setTabSelectedListener(this);
     }
 
     @Override
@@ -90,5 +93,37 @@ public class MainActivity extends BaseActivity {
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onTabSelected(int position) {
+        String msg = "";
+        switch (position) {
+            case 0:
+                msg = "首页";
+                break;
+            case 1:
+                msg = "行情";
+                break;
+            case 2:
+                msg = "交易";
+                break;
+            case 3:
+                msg = "我的";
+                break;
+            default:
+                break;
+        }
+        showShortToast(msg);
+    }
+
+    @Override
+    public void onTabUnselected(int position) {
+
+    }
+
+    @Override
+    public void onTabReselected(int position) {
+
     }
 }
