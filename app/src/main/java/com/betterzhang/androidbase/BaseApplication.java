cea15ed579178ca9400b0614ce9ac202ab9c6486
BetterZhang <betterzhang.dev@gmail.com>
2017-08-04 13:54:47 +0800

    add BaseApplication

---
+++ app/src/main/java/com/betterzhang/androidbase/BaseApplication.java
@@ -0,0 +1,27 @@
+package com.betterzhang.androidbase;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by Android Studio.
+ * Author : zhangzhongqiang
+ * Email  : betterzhang.dev@gmail.com
+ * Time   : 2017/08/04 下午 1:41
+ * Desc   : BaseApplication
+ */
+
+public class BaseApplication extends Application {
+
+    private static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = this.getApplicationContext();
+    }
+
+    public static Context getCurrentAppContext() {
+        return mContext;
+    }
+}
